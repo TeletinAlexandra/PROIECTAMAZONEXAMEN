9a57348be15314f0c481d1327b030a6281fefcf8
TeletinAlexandra <56999330+TeletinAlexandra@users.noreply.github.com>
2021-04-14 22:28:01 +0300

    Add files via upload

---
+++ OrbitalElements.m
@@ -0,0 +1,41 @@
+function [ h, mag_h, i, omega, e, mag_e, w, theta ] = OrbitalElements ( r,v,muo )
+
+%% INPUTS:
+% r        :  vectorul de pozitie
+% v       :  vectorul viteza
+% muo :  parametrul gravitational
+%% OUTPUTS:
+% h              : vectorul momentului unghiular in km^2/s
+% muo        :  Constanta Gravitationala
+% e              : excentricitatea
+% i               : inclinatia
+% Omega    : longitudinea nodului ascendent
+% w              : argumentul periheliului
+% theta        : anomalia adevarata
+% ---------------------------------------------------------------------------------------------------------------------------------------------------------
+mag_r=norm(r);
+mag_v=norm(v);
+vr=dot(r,v)/mag_r;
+h=cross(r,v);
+mag_h=norm(h);
+i=acosd(h(3)/mag_h);
+N=cross([0,0,1],h);
+mag_N=norm(N);
+if N(2) >= 0
+    omega=acosd(N(1)/mag_N);
+elseif N(2) < 0
+    omega=360-acosd(N(1)/mag_N);
+end
+e=((mag_v^2-muo/mag_r)*r-mag_r*vr*v)/muo;
+mag_e=norm(e);
+if e(3) >= 0
+    w=acosd(dot(N,e)/mag_N/mag_e);
+elseif e(3) < 0
+    w=360-acosd(dot(N,e)/mag_N/mag_e);
+end
+if vr >= 0
+    theta=acosd(dot(e,r)/mag_r/mag_e);
+elseif vr < 0
+    theta=360-acosd(dot(e,r)/mag_r/mag_e);
+end
+end
---
+++ OrbitalElements2rvGeo.m
@@ -0,0 +1,26 @@
+function [ R, V ] = OrbitalElements2rvGeo( h, muo, e, i, Omega, w, theta )
+% TELETIN ALEXANDRA-IONELA 
+% GRUPA MAE1
+% 15.04.2021
+%% INPUTS:
+% h              : vectorul momentului unghiular in km^2/s
+% muo        :  Constanta Gravitationala
+% e              : excentricitatea
+% i               : inclinatia
+% Omega    : longitudinea nodului ascendent
+% w              : argumentul periheliului
+% theta        : anomalia adevarata
+%% OUTPUTS:
+% r    : vectorul de pozitie
+% v    : vecctorul viteza in km/s
+% ---------------------------------------------------------------------------------------------------------------------------------------------------------
+%r
+R_bar=h^2/muo/(1+e*cosd(theta))*[cosd(theta);sind(theta);0];
+% v
+V_bar=muo/h*[-sind(theta);e+cosd(theta);0];
+QxX=[-sind(Omega)*cosd(i)*sind(w)+cosd(Omega)*cosd(w),-sind(Omega)*cosd(i)*cosd(w)-cosd(Omega)*sind(w),sind(Omega)*sind(i);...
+           cosd(Omega)*cosd(i)*sind(w)+sind(Omega)*cosd(w),cosd(Omega)*cosd(i)*cosd(w)-sind(Omega)*sind(w),-cosd(Omega)*sind(i);...
+           sind(i)*sind(w),sind(i)*cosd(w),cosd(i)];
+R=QxX*R_bar;
+V=QxX*V_bar;
+end
